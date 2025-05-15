946bc67e1f33ae74907db6d5ccea26810da73e2b
重装小杰 <73771431+beiklive@users.noreply.github.com>
2025-05-16 07:40:44 +0800

    fix(ui_FrameLessWindow): 修复窗口边框调整逻辑并初始化UI

--- gui/widgets/ui_FrameLessWindow/ui_GripItem.cpp
+++ gui/widgets/ui_FrameLessWindow/ui_GripItem.cpp
@@ -5,6 +5,7 @@ beiklive::Ui_GripItem::Ui_GripItem(QWidget *parent, WindowParts parts, bool hide
                                                                                          m_hideColor(hideColor),
                                                                                          m_parent(parent)
 {
+    InitUI();
 }
 
 void beiklive::Ui_GripItem::setGeo(int x, int y, int w, int h)
@@ -23,25 +24,25 @@ void beiklive::Ui_GripItem::mouseMoveEvent(QMouseEvent *event)
     {
     case WindowParts::TOP:
         height = qMax(this->parentWidget()->minimumHeight(),
-                          this->parentWidget()->height() - delta.y());
+                      this->parentWidget()->height() - delta.y());
         geo.setTop(geo.bottom() - height);
         this->parentWidget()->setGeometry(geo);
         break;
     case WindowParts::BOTTOM:
         height = qMax(this->parentWidget()->minimumHeight(),
-                          this->parentWidget()->height() + delta.y());
+                      this->parentWidget()->height() + delta.y());
         this->parentWidget()->resize(this->parentWidget()->width(), height);
         break;
     case WindowParts::LEFT:
         width = qMax(this->parentWidget()->minimumWidth(),
-                         this->parentWidget()->width() - delta.x());
+                     this->parentWidget()->width() - delta.x());
 
         geo.setLeft(geo.right() - width);
         this->parentWidget()->setGeometry(geo);
         break;
     case WindowParts::RIGHT:
         width = qMax(this->parentWidget()->minimumWidth(),
-                         this->parentWidget()->width() + delta.x());
+                     this->parentWidget()->width() + delta.x());
         this->parentWidget()->resize(width, this->parentWidget()->height());
         break;
     default:
@@ -56,60 +57,76 @@ void beiklive::Ui_GripItem::InitUI()
     switch (m_parts)
     {
     case WindowParts::TOP:
+    {
         /* code */
         setGeometry(QRect(0, 5, m_parent->width(), 10));
         setMaximumHeight(10);
         setStyleSheet("background-color: rgb(154, 31, 202);");
         setCursor(QCursor(Qt::SizeVerCursor));
-        break;
+    }
+    break;
     case WindowParts::BOTTOM:
+    {
         /* code */
         setGeometry(QRect(0, m_parent->height() - 10, m_parent->width(), 10));
         setMaximumHeight(10);
         setStyleSheet("background-color: rgb(154, 31, 202);");
         setCursor(QCursor(Qt::SizeVerCursor));
         break;
+    }
     case WindowParts::LEFT:
+    {
         /* code */
         setGeometry(QRect(0, 5, 10, m_parent->height() - 10));
         setMaximumWidth(10);
         setStyleSheet("background-color: rgb(154, 31, 202);");
         setCursor(QCursor(Qt::SizeHorCursor));
         break;
+    }
     case WindowParts::RIGHT:
+    {
         /* code */
         setGeometry(QRect(m_parent->width() - 10, 5, 10, m_parent->height() - 10));
         setMaximumWidth(10);
         setStyleSheet("background-color: rgb(154, 31, 202);");
         setCursor(QCursor(Qt::SizeHorCursor));
         break;
+    }
     case WindowParts::TOP | WindowParts::LEFT:
+    {
         m_grip = new QSizeGrip(this);
         m_grip->setFixedSize(size());
         setGeometry(QRect(5, 5, 15, 15));
         setStyleSheet("background-color: #333; border: 2px solid #55FF00;");
         break;
+    }
     case WindowParts::TOP | WindowParts::RIGHT:
+    {
         /* code */
         m_grip = new QSizeGrip(this);
         m_grip->setFixedSize(size());
         setGeometry(QRect(m_parent->width() - 20, 5, 15, 15));
         setStyleSheet("background-color: #333; border: 2px solid #55FF00;");
         break;
+    }
     case WindowParts::BOTTOM | WindowParts::LEFT:
+    {
         /* code */
         m_grip = new QSizeGrip(this);
         m_grip->setFixedSize(size());
         setGeometry(QRect(5, m_parent->height() - 20, 15, 15));
         setStyleSheet("background-color: #333; border: 2px solid #55FF00;");
         break;
+    }
     case WindowParts::BOTTOM | WindowParts::RIGHT:
+    {
         /* code */
         m_grip = new QSizeGrip(this);
         m_grip->setFixedSize(size());
         setGeometry(QRect(m_parent->width() - 20, m_parent->height() - 20, 15, 15));
         setStyleSheet("background-color: #333; border: 2px solid #55FF00;");
         break;
+    }
     default:
         break;
     }
